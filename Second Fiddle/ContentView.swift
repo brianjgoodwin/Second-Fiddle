43cd8467df735dd9193d77ca2fe13fcc45bc293c
Brian Goodwin <brianjgoodwin@gmail.com>
2025-06-26 14:56:43 -0400

    Initial Commit

---
+++ Second Fiddle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Second Fiddle
+//
+//  Created by Brian on 6/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Second Fiddle/Second_FiddleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Second_FiddleApp.swift
+//  Second Fiddle
+//
+//  Created by Brian on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Second_FiddleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
